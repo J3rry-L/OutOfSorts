811b92c2b3fc4200be505a2abd44e520ee93a1e6
J3rry-L <jliang21@stuy.edu>
2020-12-15 13:19:57 -0500

    Tested with first test case

--- Tester.java
+++ Tester.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Tester{
   public static void main(String args[]){
-    
+    int [] array1 = new int[]{5,1,12,-5,16};
+    Sorts.bubbleSort(array1);
+    System.out.println(Arrays.toString(array1));
   }
 }
