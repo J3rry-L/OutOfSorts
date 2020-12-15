4d5c0149afc98975b488b7894dc13ca3901db57b
J3rry-L <jliang21@stuy.edu>
2020-12-15 13:19:26 -0500

    Created bubbleSort method

--- Sorts.java
+++ Sorts.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Sorts{
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+  public static void bubbleSort(int[] data){
+    boolean sorted = false;
+    while (!sorted){
+      sorted = true;
+      int temp;
+      for (int i = 0; i < data.length - 1; i++){
+        if (data[i] > data[i+1]){
+          sorted = false;
+          temp = data[i+1];
+          data[i+1] = data[i];
+          data[i] = temp;
+        }
+        //System.out.println(Arrays.toString(data));
+      }
+    }
+  }
 }
